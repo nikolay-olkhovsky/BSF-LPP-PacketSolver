b8330e6aafe33045c9c1ec1049000d9ff26a33ed
Leonid Sokolinsky <Leonid.Sokolinsky@susu.ru>
2020-11-06 14:39:43 +0500

    Add files via upload

--- Problem-bsfParameters.h
+++ Problem-bsfParameters.h
@@ -13,8 +13,6 @@ This source code has been produced with using BSF-skeleton
 //#define PP_BSF_ITER_OUTPUT		// If PP_BSF_ITER_OUTPUT is defined then Iteration Output is performed
 #define PP_BSF_TRACE_COUNT 1	// Each PP_BSF_TRACE_COUNT-th iteration to be outputted
 #define PP_BSF_MAX_JOB_CASE 2
-#define PP_BSF_FRAGMENTED_MAP_LIST	// If it is defined, the worker-node stores only its part of the map-list. 
-									// Otherwise, the map-list is replicated on each worker-node. 
 //--------------------------- OpenMP Parameters ---------------------------
 //#define PP_BSF_OMP				// If PP_BSF_OMP is defined then OpenMP is turned on for Map Step
 //#define PP_BSF_NUM_THREADS 12	// If PP_BSF_NUM_THREADS is udefined then all accessable threads are used
