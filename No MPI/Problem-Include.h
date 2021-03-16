527e8c2a107160c063cb1b0adaa4af5e17d9b990
Leonid Sokolinsky <Leonid.Sokolinsky@susu.ru>
2021-03-16 08:18:42 +0500

    Add files via upload

---
+++ No MPI/Problem-Include.h
@@ -0,0 +1,14 @@
+/*==============================================================================
+Project: LiFe
+Theme: Apex Method (Predictor + Corrector)
+Module: Problem-Include.h ("Includ" Files of the Problem)
+Author(s): Leonid B. Sokolinsky, Irina M. Sokolinskaya
+==============================================================================*/
+#pragma once
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <string>
